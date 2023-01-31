c552da91b91846de953e894f474102156d7e8fbd
bjorn <bjorn.umera@gmail.com>
2023-01-31 17:06:43 +0200

    init

---
+++ server/src/main/java/com/example/TicketGuru/TicketGuruApplication.java
@@ -0,0 +1,13 @@
+package com.example.TicketGuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketGuruApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/TicketGuru/TicketGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TicketGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
