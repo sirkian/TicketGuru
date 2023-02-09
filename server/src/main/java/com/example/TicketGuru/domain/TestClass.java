4527120639b22a1d7172043d3091ca0e033a45a8
Sonja <sonja@scoundrel.fi>
2023-02-09 23:58:18 +0200

    Testikommentti lisätty testiluokkaan

--- server/src/main/java/com/example/TicketGuru/domain/TestClass.java
+++ server/src/main/java/com/example/TicketGuru/domain/TestClass.java
@@ -3,4 +3,6 @@ package com.example.TicketGuru.domain;
 public class TestClass {
 	// Testataan tällä, että kaikilla on kehitysympäristö kondiksessa.
 	// Lisätkää vaikka kommentti jonon jatkoksi ja puskekaa gittiin. Poistetaan nää testiluokat sitten kun kaikilla ok.
+	
+	//Hyvin toimii! -Sonja
 }
