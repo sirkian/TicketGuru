8f9ae7bdd80bba01dc223800d08105a960131143
Sonja <sonja@scoundrel.fi>
2023-02-11 21:45:32 +0200

    Muutoksia AppUser-luokassa

--- server/src/main/java/com/example/TicketGuru/domain/AppUser.java
+++ server/src/main/java/com/example/TicketGuru/domain/AppUser.java
@@ -1,13 +1,14 @@
 package com.example.TicketGuru.domain;
 
-import java.util.HashMap;
-
 import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 
@@ -19,35 +20,42 @@ public class AppUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id", nullable = false, updatable = false)
-	private Long TypeId;
+	private Long userId;
 	
 	//Miten viiteavaimet merkitään?
-	@Column(name = "role_id", nullable = false, updatable = false)
-	private Long TypeRefId;
+	//Yhdelle käyttäjälle vain yksi rooli
+	//JsonIgnore
+	//ei konstruktoriin eikä toString
+	(name = "role_id", nullable = false, updatable = false)
+	private Long roleId;
 	
 	@Column(name="first_name", length = 50, nullable = false, updatable = true)
-	private String FirstName;
+	private String firstName;
 	
 	@Column(name="last_name", length = 50, nullable = false, updatable = true)
-	private String LastName;
+	private String lastName;
 	
 	@Column(name="email", length = 150, nullable = false, updatable = true)
-	private String Email;
+	private String email;
 	
 	//Mikä olisi hyvä tietotyyppi salasanalle?
 	@Column(name="password_hash", length = 450, nullable = false, updatable = true)
-	private String Password;
+	private String password;
 	
 	//Asetin tietotyypiksi stringin, jos halutaan käyttäjältä suuntanumero (+358..)
 	@Column(name="phone_num", length = 15, nullable = false, updatable = true)
-	private String PhoneNum;
+	private String phoneNum;
 	
 	@Column(name="details", length = 450, nullable = true, updatable = true)
-	private String Details;
+	private String details;
 	
 	@Column(name="address", length = 250, nullable = false, updatable = true)
-	private String Address;
+	private String address;
 	
 	@Column(name="postal_code", length = 10, nullable = false, updatable = false)
-	private int PostalCode;
+	private int postalCode;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "role_id") 
+	private Role role;
 }
