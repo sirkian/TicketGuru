262dac21d094bae8fd66f729a0e483926eb08471
Salla Estrada <estradasalla@gmail.com>
2023-02-11 17:12:56 +0200

    Testausta varten tehty muutos

--- server/src/main/java/com/example/TicketGuru/domain/TestClass.java
+++ server/src/main/java/com/example/TicketGuru/domain/TestClass.java
@@ -3,4 +3,5 @@ package com.example.TicketGuru.domain;
 public class TestClass {
 	// Testataan tällä, että kaikilla on kehitysympäristö kondiksessa.
 	// Lisätkää vaikka kommentti jonon jatkoksi ja puskekaa gittiin. Poistetaan nää testiluokat sitten kun kaikilla ok.
+	// Tämä on Sallan testikommentti
 }
