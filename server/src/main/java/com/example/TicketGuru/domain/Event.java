d9146d7378f7eed842b81827b50301387567f7b9
Milla Laaja <millamlaaja@gmail.com>
2023-02-11 09:22:17 +0200

    event luokkaan lisätty yhteydet lippuun issue #43

--- server/src/main/java/com/example/TicketGuru/domain/Event.java
+++ server/src/main/java/com/example/TicketGuru/domain/Event.java
@@ -1,14 +1,20 @@
 package com.example.TicketGuru.domain;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 
 @Entity
@@ -16,11 +22,13 @@ public class Event {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "event_id", nullable = false)
 	private Long eventId;
 	
-	@Column(name = "event_name")
+	@Column(name = "event_name", length=100)
 	private String eventName;
 	
+	@Column(length = 450)
 	private String description;
 	
 	@Column(name = "start_time")
@@ -29,25 +37,71 @@ public class Event {
 	@Column(name = "end_time")
 	private LocalDate endTime;
 	
+	@Column(length = 250)
 	private String address;
 	
 	@Column(name = "amount_tickets")
 	private int amountTickets;
 	
 	// postalcode- luokkaa ei ole vielä, yhteys one-to-many -> eventissä yksi postinro
-	// yhtä postinumeroa vastaa usea postal code
+	// yhtä postinumeroa vastaa usea event
 	//ManyToOne
-	//@ManyToOne
+	//@ManyToOne(fetch = FetchType.EAGER)
 	//@JoinColumn(name="postal_code")
 	
 	@Column(name = "postal_code")
 	private String postalCode;
+	
+	@JsonIgnore // to prevent infinite loop in rest service
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "ticketId")
+	private List<Ticket> tickets;
+	
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "ticketType")
+	private List<TicketType> ticket_types;
 
+
+	public List<Ticket> getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(List<Ticket> tickets) {
+		this.tickets = tickets;
+	}
 
 	public Event() {
 		super();
 	}
 
+	// konstruktori testikäyttöön, voi lisätä olion nimellä
+	
+	public Event(String eventName) {
+		super();
+		this.eventName = eventName;
+	}
+	
+
+	public Event(String eventName, String description, LocalDate startTime, LocalDate endTime, String address,
+			int amountTickets, String postalCode, List<Ticket> tickets, List<TicketType> ticket_types) {
+		super();
+		this.eventName = eventName;
+		this.description = description;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.address = address;
+		this.amountTickets = amountTickets;
+		this.postalCode = postalCode;
+		this.tickets = tickets;
+		this.ticket_types = ticket_types;
+	}
+
+	public List<TicketType> getTicket_types() {
+		return ticket_types;
+	}
+
+	public void setTicket_types(List<TicketType> ticket_types) {
+		this.ticket_types = ticket_types;
+	}
 
 	public Long getEventId() {
 		return eventId;
@@ -128,14 +182,13 @@ public class Event {
 		this.postalCode = postalCode;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Event [eventId=" + eventId + ", eventName=" + eventName + ", description=" + description
 				+ ", startTime=" + startTime + ", endTime=" + endTime + ", address=" + address + ", amountTickets="
-				+ amountTickets + ", postalCode=" + postalCode + "]";
+				+ amountTickets + ", postalCode=" + postalCode + ", tickets=" + tickets + ", ticket_types="
+				+ ticket_types + "]";
 	}
-	
-	
 
+	
 }
