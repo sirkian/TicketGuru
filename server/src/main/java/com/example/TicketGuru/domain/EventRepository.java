1ab57712a5b559a737ecab8864520609e818ef9f
sirkian <anssisirkia@gmail.com>
2023-02-22 11:16:32 +0200

    Lisätty tapahtuman hakeminen nimellä #20

--- server/src/main/java/com/example/TicketGuru/domain/EventRepository.java
+++ server/src/main/java/com/example/TicketGuru/domain/EventRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 public interface EventRepository extends CrudRepository<Event, Long> {
 
 	Optional<Event> findById(Long eventId);
+	
+	// Hakee tapahtumat, joiden nimi sisältää hakusanan, välittämättä kirjainten koosta
+	// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+	Iterable<Event> findByEventNameContainingIgnoreCase(String eventName);
 }
--- server/src/main/java/com/example/TicketGuru/web/EventRestController.java
+++ server/src/main/java/com/example/TicketGuru/web/EventRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.TicketGuru.domain.Event;
@@ -25,9 +26,16 @@ public class EventRestController {
 		return eventRepository.findAll();	
 	}
 	
+	// Palauttaa tapahtuman haetulla id:llä
 	@GetMapping("/events/{eventId}")
 	public Optional<Event> getEvent(@PathVariable("eventId") Long eventId) {
 		return eventRepository.findById(eventId);	
+	}
+	
+	// Palauttaa tapahtumat joiden nimi sisältää hakusanan
+	@GetMapping("/events/search")
+	public Iterable<Event> getEventByName(@RequestParam(value = "name") String name) {
+		return eventRepository.findByEventNameContainingIgnoreCase(name);
 	}
 	
 	// Muokkaa valittua tapahtumaa, ei vielä toimi
