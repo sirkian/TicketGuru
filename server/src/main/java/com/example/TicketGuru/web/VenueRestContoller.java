67d63c7bda6c355b998f5da4e043b81af2c0cac5
Milla Laaja <millamlaaja@gmail.com>
2023-02-24 23:00:27 +0200

    venue PUT metodi tehty issue #64

--- server/src/main/java/com/example/TicketGuru/web/VenueRestContoller.java
+++ server/src/main/java/com/example/TicketGuru/web/VenueRestContoller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,9 +32,17 @@ public class VenueRestContoller {
 	}
 	
 	// lisää uuden tapahtumapaikan
-	@PostMapping("venues")
+	@PostMapping("/venues")
 	Venue newVenue(@RequestBody Venue newVenue) {
 		return venueRepository.save(newVenue);
+	}
+	
+	// muokkaa tapahtumapaikkaa, jolla valittu venueId
+	// jos tapahtumapaikkaa ei löydy ID:llä, metodi luo uuden tapahtuman valitulla id:llä
+	@PutMapping("/venues/{venueId}")
+	Venue replaceVenue(@RequestBody Venue editedVenue, @PathVariable("venueId") Long venueId) {
+		editedVenue.setVenueId(venueId);
+		return venueRepository.save(editedVenue);
 	}
 	
 }
