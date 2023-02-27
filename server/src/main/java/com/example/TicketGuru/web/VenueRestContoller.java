81a659fb106b1750e9ee28b647d6cd22dbd3aeb6
Salla Estrada <estradasalla@gmail.com>
2023-02-27 15:05:16 +0200

    Metodi tapahtumapaikan poistamiselle #70

--- server/src/main/java/com/example/TicketGuru/web/VenueRestContoller.java
+++ server/src/main/java/com/example/TicketGuru/web/VenueRestContoller.java
@@ -3,13 +3,17 @@ package com.example.TicketGuru.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.TicketGuru.domain.Event;
 import com.example.TicketGuru.domain.Venue;
 import com.example.TicketGuru.domain.VenueRepository;
 
@@ -44,5 +48,11 @@ public class VenueRestContoller {
 		editedVenue.setVenueId(venueId);
 		return venueRepository.save(editedVenue);
 	}
-	
+	//Poistaa tapahtumapaikan id:n perusteella
+	@DeleteMapping(value = "/delete_venue/{venueId}")
+	public Iterable<Venue> deleteVenue(@PathVariable("venueId") Long venueId) {
+		venueRepository.deleteById(venueId);
+		return venueRepository.findAll();
+		}
+		
 }
