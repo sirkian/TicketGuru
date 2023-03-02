a5ac47cf9c0e90ae06578d14275f1e85ef3bc97d
Erika Koskinen <bgx093@myy.haaga-helia.fi>
2023-03-02 22:08:25 +0200

    Muokattu tapahtuman poiston polku #77

--- server/src/main/java/com/example/TicketGuru/web/EventRestController.java
+++ server/src/main/java/com/example/TicketGuru/web/EventRestController.java
@@ -3,13 +3,12 @@ package com.example.TicketGuru.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -63,7 +62,7 @@ public class EventRestController {
 	}
 	
 	//Poistaa tapahtuman id:n perusteella
-	@RequestMapping(value = "/delete_event/{eventId}", method = RequestMethod.GET)
+	@DeleteMapping("/events/{eventId}")
 	public Iterable<Event> deleteEvent(@PathVariable("eventId") Long eventId) {
 		eventRepository.deleteById(eventId);
 		return eventRepository.findAll();
