0c58d56fb12800201d763409cc7cee344e1d623d
Sonja <sonja@scoundrel.fi>
2023-03-04 23:19:10 +0200

    #85 Role-rajapinta lisätty

---
+++ server/src/main/java/com/example/TicketGuru/web/RoleRestController.java
@@ -0,0 +1,39 @@
+package com.example.TicketGuru.web;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.TicketGuru.domain.Role;
+import com.example.TicketGuru.domain.RoleRepository;
+
+@RestController
+public class RoleRestController {
+
+	@Autowired
+	RoleRepository rolerepository;
+	
+	// Palauttaa id:llä haetun roolin
+		@GetMapping("/roles/{roleId}")
+		public Optional<Role> getRole(@PathVariable("roleId") Long roleId) {
+			return rolerepository.findById(roleId);
+		}
+		
+	//Palauttaa kaikki roolit
+		@GetMapping("/roles")
+		public Iterable<Role> getAllRoles() {
+			return rolerepository.findAll();
+		}
+		
+	//Lisää roolin
+		@PostMapping("/roles")
+		Role newRole(@RequestBody Role newRole) {
+			return rolerepository.save(newRole);
+		}
+		
+	
+}
