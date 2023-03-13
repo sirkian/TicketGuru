6b4322eda0cad8770cbd3e3331bd8f5effcc07e5
Erika Koskinen <bgx093@myy.haaga-helia.fi>
2023-03-13 19:37:28 +0200

    RoleRestControllerin vastauskoodit #90

--- server/src/main/java/com/example/TicketGuru/web/RoleRestController.java
+++ server/src/main/java/com/example/TicketGuru/web/RoleRestController.java
@@ -3,11 +3,15 @@ package com.example.TicketGuru.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.example.TicketGuru.domain.Role;
 import com.example.TicketGuru.domain.RoleRepository;
 
@@ -22,6 +26,10 @@ public class RoleRestController {
 	// Palauttaa id:llä haetun roolin
 		@GetMapping("/roles/{roleId}")
 		public Optional<Role> getRole(@PathVariable("roleId") Long roleId) {
+			Optional<Role> role = rolerepository.findById(roleId);
+			if (role.isEmpty()) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Annetulla id:llä ei löytynyt yhtään roolia");
+			}
 			return rolerepository.findById(roleId);
 		}
 		
@@ -33,8 +41,13 @@ public class RoleRestController {
 		
 	//Lisää roolin
 		@PostMapping("/roles")
+		@ResponseStatus(HttpStatus.CREATED)
 		public Role newRole(@Valid @RequestBody Role newRole) {
-			return rolerepository.save(newRole);
+			try {
+				return rolerepository.save(newRole);
+			} catch (Exception e) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+			}
 		}
 		
 	
