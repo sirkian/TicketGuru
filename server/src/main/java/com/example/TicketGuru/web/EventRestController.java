0776028e5de04c2a16547e6692a6b5c222eea4a5
sirkian <anssisirkia@gmail.com>
2023-03-13 13:26:13 +0200

    Endpoint jäljellä olevien lippujen määrälle #82

--- server/src/main/java/com/example/TicketGuru/web/EventRestController.java
+++ server/src/main/java/com/example/TicketGuru/web/EventRestController.java
@@ -23,23 +23,22 @@ import com.example.TicketGuru.domain.VenueRepository;
 
 import jakarta.validation.Valid;
 
-
 @RestController
 public class EventRestController {
-	
+
 	@Autowired
 	private EventRepository eventRepository;
-	
+
 	@Autowired
 	private VenueRepository venueRepository;
-	
+
 	// Palauttaa kaikki järjestelmään tallennetut tapahtumat
 	@GetMapping("/events")
 	// 200 OK ja tyhjä taulukko jos ei ole tapahtumia
 	public Iterable<Event> getAllEvents() {
 		return eventRepository.findAll();
 	}
-	
+
 	// Palauttaa tapahtuman haetulla id:llä
 	@GetMapping("/events/{eventId}")
 	public Optional<Event> getEvent(@PathVariable("eventId") Long eventId) {
@@ -49,7 +48,7 @@ public class EventRestController {
 		}
 		return event;
 	}
-	
+
 	// Palauttaa tapahtumat joiden nimi sisältää hakusanan
 	@GetMapping("/events/q")
 	public Iterable<Event> getEventsByName(@RequestParam(value = "name") String name) {
@@ -59,21 +58,22 @@ public class EventRestController {
 		}
 		return events;
 	}
-	
+
 	// Palauttaa haetun tapahtumapaikan kaikki tapahtumat
 	// Korvataan endpoint suoralla vastauksella (linkit poies)
 	@GetMapping("/venues/{venueId}/events")
-	public Iterable<Event> getEventsByVenue(@PathVariable("venueId") Long venueId) {	
+	public Iterable<Event> getEventsByVenue(@PathVariable("venueId") Long venueId) {
 		Optional<Venue> venue = venueRepository.findById(venueId);
 		if (venue.isEmpty()) {
-			// Heitetään 404, mikäli annetulla idllä ei ole tapahtumapaikkaa (esim. /venues/99/events)
+			// Heitetään 404, mikäli annetulla idllä ei ole tapahtumapaikkaa (esim.
+			// /venues/99/events)
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tapahtumapaikkaa ei ole olemassa");
 		}
-		return eventRepository.findByVenue(venue);	
-			
+		return eventRepository.findByVenue(venue);
+
 	}
-	
-	//lisää uuden tapahtuman
+
+	// lisää uuden tapahtuman
 	@PostMapping("/events")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Event newEvent(@Valid @RequestBody Event newEvent) {
@@ -83,7 +83,7 @@ public class EventRestController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-	
+
 	// Muokkaa id:llä valittua tapahtumaa
 	@PutMapping("/events/{eventId}")
 	public Event editEvent(@Valid @RequestBody Event editedEvent, @PathVariable("eventId") Long eventId) {
@@ -93,33 +93,44 @@ public class EventRestController {
 		if (event.isPresent()) {
 			try {
 				editedEvent.setEventId(eventId);
-			 	return eventRepository.save(editedEvent);
+				return eventRepository.save(editedEvent);
 			} catch (Exception e) {
 				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tarkista viiteavaimet: " + e.getMessage());
-			}		
+			}
 		} else {
 			// Jos tapahtumaa ei ole olemassa, heitetään 404
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Annetulla id:llä ei ole olemassa tapahtumaa");		
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Annetulla id:llä ei ole olemassa tapahtumaa");
 		}
-		
+
 	}
-	
-	//Poistaa tapahtuman id:n perusteella
+
+	// Poistaa tapahtuman id:n perusteella
 	@DeleteMapping("/events/{eventId}")
 	public Iterable<Event> deleteEvent(@PathVariable("eventId") Long eventId) {
 		try {
 			eventRepository.deleteById(eventId);
 		} catch (Exception e) {
-			// Heitetään 404 jos poisto epäonnistui, tuskin muita syitä kun olematon id 
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Annetulla id:llä ei ole olemassa tapahtumaa");		
+			// Heitetään 404 jos poisto epäonnistui, tuskin muita syitä kun olematon id
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Annetulla id:llä ei ole olemassa tapahtumaa");
 		}
 		List<Event> events = (List<Event>) eventRepository.findAll();
 		if (events.isEmpty()) {
-			// Jos poistettiin viimeinen tapahtuma, palautetaan 204 
+			// Jos poistettiin viimeinen tapahtuma, palautetaan 204
 			throw new ResponseStatusException(HttpStatus.NO_CONTENT);
 		}
 		return events;
 	}
-	
-	
+
+	// Hakee tapahtuman kokonaislippumäärän ja vähentää myytyjen lippujen määrän
+	// Palauttaa tuloksen kokonaislukuna
+	@GetMapping("/events/{eventId}/ticketsleft")
+	public Integer getTicketsLeftByEvent(@PathVariable("eventId") Long eventId) {
+		try {
+			return eventRepository.findById(eventId).get().getAmountTickets()
+					- eventRepository.getAmountOfSoldTickets(eventId);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Annetulla id:llä ei ole olemassa tapahtumaa");
+		}
+	}
+
 }
