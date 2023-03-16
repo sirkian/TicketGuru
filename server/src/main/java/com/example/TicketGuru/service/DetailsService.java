ef15db780dd6f471bf5e588f12e146fa70111fa1
Erika Koskinen <bgx093@myy.haaga-helia.fi>
2023-03-16 14:32:42 +0200

    Korjattu DetailsService #117

--- server/src/main/java/com/example/TicketGuru/service/DetailsService.java
+++ server/src/main/java/com/example/TicketGuru/service/DetailsService.java
@@ -1,48 +1,24 @@
 package com.example.TicketGuru.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-=======
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.core.userdetails.User.UserBuilder;
->>>>>>> e7f317a54caa7517cab18021957f514e16a45b6f
 import org.springframework.stereotype.Service;
 
 import com.example.TicketGuru.domain.AppUser;
 import com.example.TicketGuru.domain.AppUserRepository;
 
-<<<<<<< HEAD
-
-@Service
-public class DetailsService implements UserDetailsService {
-	private final AppUserRepository appusers;
-	
-=======
 @Service
 public class DetailsService implements UserDetailsService {
 
 	private final AppUserRepository appusers;
 
->>>>>>> e7f317a54caa7517cab18021957f514e16a45b6f
 	@Autowired
 	public DetailsService(AppUserRepository appusers) {
 		this.appusers = appusers;
 	}
-<<<<<<< HEAD
-	
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		AppUser appuser = appusers.findByEmail(email);
-		
-		UserBuilder builder = null;
-		
-=======
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -50,7 +26,6 @@ public class DetailsService implements UserDetailsService {
 
 		UserBuilder builder = null;
 
->>>>>>> e7f317a54caa7517cab18021957f514e16a45b6f
 		if (appuser == null) {
 			throw new UsernameNotFoundException("Käyttäjää " + email + " ei löydetty!");
 		} else {
@@ -59,14 +34,7 @@ public class DetailsService implements UserDetailsService {
 			builder.roles(appuser.getAppUserRoles().toString());
 			System.out.println(appuser);
 		}
-<<<<<<< HEAD
-		else {
-			builder = org.springframework.security.core.userdetails.User.withUsername(email);
-			builder.password(appuser.getPassword());
-			builder.roles(appuser.getAppUserRoles().toString());
-		}
-=======
->>>>>>> e7f317a54caa7517cab18021957f514e16a45b6f
+
 		return builder.build();
 	}
 
