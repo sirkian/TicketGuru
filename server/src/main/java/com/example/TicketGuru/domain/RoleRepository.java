d83df519ffd3db56ae076f5ff56c37817fcad372
Erika Koskinen <bgx093@myy.haaga-helia.fi>
2023-03-24 14:39:47 +0200

    JUnit-testi RoleRepositorylle #125

--- server/src/main/java/com/example/TicketGuru/domain/RoleRepository.java
+++ server/src/main/java/com/example/TicketGuru/domain/RoleRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface RoleRepository extends CrudRepository<Role, Long> {
 
 	Optional<Role> findById(Long roleID);
+	
+	// JUnit testiä varten luotu haku roolin nimellä
+	Role findByRole(String role);
 }
