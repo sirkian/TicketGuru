284caac3046365bb97221c5eb85c0510631e6e4f
Milla Laaja <millamlaaja@gmail.com>
2023-05-05 15:55:20 +0300

    raportin endpoint, luokka ja sql #25

---
+++ server/src/main/java/com/example/TicketGuru/domain/Report.java
@@ -0,0 +1,60 @@
+package com.example.TicketGuru.domain;
+
+public class Report {
+
+    // luokka, jossa kerätään raportin tiedot
+
+    private String event;
+
+    private String eventTicketType;
+
+    private int amountSoldTickets;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public String getEventTicketType() {
+        return eventTicketType;
+    }
+
+    public double getAmountSoldTickets() {
+        return amountSoldTickets;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    private double price;
+
+    private double total;
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public void setEventTicketType(String eventTicketType) {
+        this.eventTicketType = eventTicketType;
+    }
+
+    public void setAmountSoldTickets(int amountSoldTickets) {
+        this.amountSoldTickets = amountSoldTickets;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setTotal(double price, int amountSoldTickets) {
+        total = price * amountSoldTickets;
+        //this.total = total;
+    }
+
+    
+    
+}
